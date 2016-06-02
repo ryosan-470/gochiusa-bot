7a5524fb45d2e12f3610af9a062a4969bf322998
Ryosuke Sato <rskjtwp@gmail.com>
2016-06-03 00:41:29 +0900

    Add test for javascript travis linter (Failed)

--- travis_test.sh
+++ travis_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# TRAVIS COFFEELINT TEST
+# TRAVIS LINT TEST
 COFFEE_FILES=$(find scripts -type f -name '*.coffee')
-
+JAVASCRIPT_FILES=$(find scripts -type f -name '*.js')
 return_val=0;
 
 coffeelint $COFFEE_FILES
@@ -15,9 +15,21 @@ for f in $COFFEE_FILES ; do
     $cmd >/dev/null
     if [ $? -ne 0 ]; then
         echo "Failed to compile $f"
-        exit ${return_val}
+        exit 1
     fi
     echo " # √"
 done
 
+echo ""
+echo "Javascript linter test"
+for f in $JAVASCRIPT_FILES; do
+    cmd="eslint $f"
+    echo -e "$cmd"
+    $cmd >/dev/null
+    if [ $? -ne 0 ]; then
+        echo "Failed to lint $f"
+        exit 1
+    fi
+    echo " # √"
+done
 exit ${return_val}
